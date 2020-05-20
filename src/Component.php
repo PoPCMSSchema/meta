aaee13af4b4ec7012fe4d0b028c0a8087c8fedcb
Leonardo Losoviz <leo@getpop.org>
2020-05-20 18:11:16 +0800

    Initialize Component manually

---
+++ src/Component.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Meta;
+
+use PoP\Root\Component\AbstractComponent;
+
+/**
+ * Initialize component
+ */
+class Component extends AbstractComponent
+{
+    // const VERSION = '0.1.0';
+
+    public static function getDependedComponentClasses(): array
+    {
+        return [
+            \PoP\Engine\Component::class,
+        ];
+    }
+
+    public static function getDependedMigrationPlugins(): array
+    {
+        return [
+            'migrate-meta',
+        ];
+    }
+}
